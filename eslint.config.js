bd67b0b23751976b3581bd35a2ae6d4e51d17884
Petr Schmidt <41199113+petrschmidt@users.noreply.github.com>
2025-07-15 16:36:40 +0200

    add `@tanstack/react-query`, `vitest`, update eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,19 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
+import pluginQuery from '@tanstack/eslint-plugin-query';
 
 export default tseslint.config([
   globalIgnores(['dist']),
   {
+    plugins: { '@tanstack/query': pluginQuery },
     files: ['**/*.{ts,tsx}'],
+    rules: {
+      '@tanstack/query/exhaustive-deps': 'error',
+    },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
@@ -20,4 +25,4 @@ export default tseslint.config([
       globals: globals.browser,
     },
   },
-])
+]);
