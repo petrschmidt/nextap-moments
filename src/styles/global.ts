ee8ef32beb38ba28bc110081f735b6e8e7258b25
Petr Schmidt <41199113+petrschmidt@users.noreply.github.com>
2025-07-21 18:05:08 +0200

    refactor: add global `button` reset

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -9,4 +9,17 @@ export const GlobalStyle = createGlobalStyle`
   body * {
     box-sizing: border-box;
   }
+  
+  button {
+    -webkit-appearance: none;
+    border-radius: 0;
+    text-align: inherit;
+    background: none;
+    box-shadow: none;
+    padding: 0;
+    cursor: pointer;
+    border: none;
+    color: inherit;
+    font: inherit;
+  }
 `;
