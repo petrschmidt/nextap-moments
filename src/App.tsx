4b416ccd1cb466220c351bd0726151ce16a813d8
Petr Schmidt <41199113+petrschmidt@users.noreply.github.com>
2025-07-21 18:04:19 +0200

    refactor: update font imports

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,10 @@ import { theme } from './styles/theme.ts';
 import { GlobalStyle } from './styles/global.ts';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-import '@fontsource/poppins';
-import '@fontsource-variable/nunito';
+import '@fontsource/poppins/400.css';
+import '@fontsource/poppins/500.css';
+import '@fontsource/poppins/600.css';
+import '@fontsource/poppins/700.css';
 
 const queryClient = new QueryClient();
 
