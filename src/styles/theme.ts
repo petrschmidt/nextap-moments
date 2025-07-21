75f734fd3bada4c79a34001f1642be9a711bcd0e
Petr Schmidt <41199113+petrschmidt@users.noreply.github.com>
2025-07-21 18:05:29 +0200

    refactor: rewrite media query helpers

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,17 +1,4 @@
-const BREAKPOINTS = {
-  xs: '0px',
-  sm: '576px',
-  md: '768px',
-  lg: '992px',
-  xl: '1200px',
-  xxl: '1400px',
-};
-
-type Breakpoint = keyof typeof BREAKPOINTS;
-
-const mediaQueryMinFn = (breakpoint: Breakpoint): string => {
-  return `@media (min-width: ${BREAKPOINTS[breakpoint]})`;
-};
+import { mediaQueryMinFn } from './utils.ts';
 
 export const theme = {
   breakpoints: {
--- src/styles/utils.ts
+++ src/styles/utils.ts
@@ -7,3 +7,28 @@ const REM_BASE: number = 16;
 export const rem = (...pixels: number[]): string => {
   return pixels.map((val) => `${val / REM_BASE}rem`).join(' ');
 };
+
+const BREAKPOINTS = {
+  sm: '576px',
+  md: '768px',
+  lg: '992px',
+  xl: '1200px',
+  xxl: '1400px',
+};
+const BREAKPOINTS_ENTRIES = Object.entries(BREAKPOINTS);
+
+export type Breakpoint = keyof typeof BREAKPOINTS;
+
+export const mediaQueryMinFn = (
+  breakpoint: Breakpoint,
+  direction: 'min' | 'max' = 'min'
+): string => {
+  return `@media (${direction}-width: ${BREAKPOINTS[breakpoint]})`;
+};
+
+type MediaQuery = Record<Breakpoint, string>;
+
+export const media = BREAKPOINTS_ENTRIES.reduce(
+  (prev, [key]) => ({ ...prev, [key]: mediaQueryMinFn(key as Breakpoint, 'min') }),
+  {} as MediaQuery
+);
