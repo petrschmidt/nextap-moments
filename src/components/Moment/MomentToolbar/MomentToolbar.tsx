bb49a2bab57738fddfb24b1cb878697a1a931e21
Petr Schmidt <41199113+petrschmidt@users.noreply.github.com>
2025-07-23 17:52:06 +0200

    refactor: update styles

--- src/components/Moment/MomentToolbar/MomentToolbar.tsx
+++ src/components/Moment/MomentToolbar/MomentToolbar.tsx
@@ -10,8 +10,14 @@ import {
   SpeakerSimpleHighIcon,
   SpeakerSimpleXIcon,
 } from '@phosphor-icons/react';
-import styled from 'styled-components';
-import { media, rem } from '../../../styles';
+import {
+  MomentButtonsContainer,
+  MomentContainer,
+  MomentIconButton,
+  MomentUserAvatarImage,
+  MomentUserContainer,
+  MomentUserName,
+} from './styles.tsx';
 
 type ActionName = 'like' | 'comment' | 'share';
 type ActionProps = {
@@ -55,113 +61,39 @@ export const MomentToolbar = ({
   const actionEntries = useMemo(() => Object.entries(socialActions), [socialActions]);
 
   return (
-    <Container>
-      <UserContainer href={`/${user.username}`}>
-        <UserAvatarImage src={user.avatarImageUrl} alt={user.username} />
-        <UserName>{user.username}</UserName>
-      </UserContainer>
+    <MomentContainer>
+      <MomentUserContainer href={`/${user.username}`}>
+        <MomentUserAvatarImage src={user.avatarImageUrl} alt={user.username} />
+        <MomentUserName>{user.username}</MomentUserName>
+      </MomentUserContainer>
       <IconContext.Provider
         value={{
           size: '1.5em',
           weight: 'fill',
         }}>
-        <ButtonsContainer>
+        <MomentButtonsContainer>
           {actionEntries.map(([actionName, { count, onClick }]) => {
             const IconComponent = ACTION_ICON_MAP[actionName as ActionName];
 
             return (
-              <IconButton key={actionName} onClick={onClick} data-test={actionName}>
+              <MomentIconButton key={actionName} onClick={onClick} data-test={actionName}>
                 <IconComponent />
                 {count}
-              </IconButton>
+              </MomentIconButton>
             );
           })}
-          <IconButton
+          <MomentIconButton
             onClick={playbackToggle.onClick}
             data-test={playbackToggle.isPaused ? 'play-button' : 'pause-button'}>
             {playbackToggle.isPaused ? <PlayIcon /> : <PauseIcon />}
-          </IconButton>
-          <IconButton
+          </MomentIconButton>
+          <MomentIconButton
             onClick={muteToggle.onClick}
             data-test={muteToggle.isMuted ? 'muted-button' : 'unmuted-button'}>
             {muteToggle.isMuted ? <SpeakerSimpleXIcon /> : <SpeakerSimpleHighIcon />}
-          </IconButton>
-        </ButtonsContainer>
+          </MomentIconButton>
+        </MomentButtonsContainer>
       </IconContext.Provider>
-    </Container>
+    </MomentContainer>
   );
 };
-
-const Container = styled.div`
-  position: relative;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin: ${({ theme }) => theme.space.md} 0;
-  user-select: none;
-  z-index: ${({ theme }) => theme.zIndex.moment.description};
-  overflow: hidden;
-`;
-
-const UserContainer = styled.a`
-  display: flex;
-  align-items: center;
-  text-decoration: none;
-  gap: ${({ theme }) => theme.space.md};
-  max-width: 30%;
-  width: auto;
-
-  ${media.sm} {
-    max-width: initial;
-  }
-`;
-
-const UserAvatarImage = styled.img`
-  width: ${rem(20)};
-  aspect-ratio: 1 / 1;
-  border-radius: ${({ theme }) => theme.radii.full};
-
-  ${media.sm} {
-    width: ${rem(32)};
-  }
-  ${media.md} {
-    width: ${rem(40)};
-  }
-`;
-
-const UserName = styled.div`
-  text-overflow: ellipsis;
-  overflow: hidden;
-  white-space: nowrap;
-  font-size: ${({ theme }) => theme.fontSizes.sm};
-  font-weight: 600;
-  color: ${({ theme }) => theme.colors.foreground.primary};
-
-  ${media.sm} {
-    font-size: ${({ theme }) => theme.fontSizes.lg};
-  }
-`;
-
-const ButtonsContainer = styled.div`
-  display: flex;
-  gap: ${({ theme }) => theme.space.md};
-  font-size: ${({ theme }) => theme.fontSizes.sm};
-
-  ${media.sm} {
-    font-size: ${({ theme }) => theme.fontSizes.lg};
-    gap: ${({ theme }) => theme.space.xl};
-  }
-`;
-
-const IconButton = styled.button`
-  display: flex;
-  align-items: center;
-  gap: ${({ theme }) => theme.space.xs};
-  font-weight: 600;
-  color: ${({ theme }) => theme.colors.foreground.primary};
-  transition: transform ${({ theme }) => theme.transitions.normal};
-
-  &:hover {
-    transform: scale(1.1);
-  }
-`;
---
+++ src/components/Moment/MomentToolbar/styles.tsx
@@ -0,0 +1,77 @@
+import styled from 'styled-components';
+import { media, rem } from '../../../styles';
+
+export const MomentContainer = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: ${({ theme }) => theme.space.md} 0;
+  user-select: none;
+  z-index: ${({ theme }) => theme.zIndex.moment.description};
+  overflow: hidden;
+`;
+
+export const MomentUserContainer = styled.a`
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  gap: ${({ theme }) => theme.space.sm};
+  max-width: 30%;
+  width: auto;
+
+  ${media.sm} {
+    max-width: initial;
+  }
+`;
+
+export const MomentUserAvatarImage = styled.img`
+  width: ${rem(20)};
+  aspect-ratio: 1 / 1;
+  border-radius: ${({ theme }) => theme.radii.full};
+
+  ${media.sm} {
+    width: ${rem(32)};
+  }
+`;
+
+export const MomentUserName = styled.div`
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+  font-size: ${({ theme }) => theme.fontSizes.sm};
+  font-weight: 600;
+  color: ${({ theme }) => theme.colors.foreground.primary};
+
+  ${media.sm} {
+    font-size: ${({ theme }) => theme.fontSizes.md};
+  }
+`;
+
+export const MomentButtonsContainer = styled.div`
+  display: flex;
+  gap: ${({ theme }) => theme.space.md};
+  font-size: ${({ theme }) => theme.fontSizes.sm};
+
+  ${media.sm} {
+    font-size: ${({ theme }) => theme.fontSizes.md};
+    gap: ${({ theme }) => theme.space.xl};
+  }
+  ${media.xl} {
+    font-size: ${({ theme }) => theme.fontSizes.lg};
+    gap: ${({ theme }) => theme.space.xl};
+  }
+`;
+
+export const MomentIconButton = styled.button`
+  display: flex;
+  align-items: center;
+  gap: ${({ theme }) => theme.space.xs};
+  font-weight: 600;
+  color: ${({ theme }) => theme.colors.foreground.primary};
+  transition: transform ${({ theme }) => theme.transitions.normal};
+
+  &:hover {
+    transform: scale(1.1);
+  }
+`;
